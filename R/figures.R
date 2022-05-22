6c41af383a42e882c3234fef9773cc67d45131bd
@tomwlo <tomwlo@gmail.com>
2022-05-22 19:52:46 +0200

    geom_area added to figure 6

--- R/figures.R
+++ R/figures.R
@@ -14,7 +14,7 @@ figure_5 <- function(){
 
 # note that the results of the Fig. 6 are subject to some small random variation
 # to avoid this effect you can set the initial value of the generator seed
-#' @importFrom purrr map_chr
+#' @importFrom purrr map_chr map walk
 #' @importFrom stringi stri_extract
 figure_6 <- function(data = experiment_course, colony_data = ant_removal,
                      sample_size = 1e4){
@@ -22,24 +22,51 @@ figure_6 <- function(data = experiment_course, colony_data = ant_removal,
                                             sample_size = sample_size)
   randomized_slopes <- calculate_regr_coeffs(data = data, colony_data = colony_data,
                                              sample_size = sample_size, randomize = TRUE)
-  check_values <- function(x){
-    interv <- HDInterval::hdi(x$slope)
-    x$slope >= interv[1] & x$slope <= interv[2]
+
+  HDI_values <- list()
+
+  calculate_HDI <- function(colony_data) {
+    # data saved in the parent frame object
+    HDI_values[[colony_data$colony[1]]] <<- HDInterval::hdi(colony_data$slope, n =500)
   }
 
-  add_hdi_mask <- . %>% split(., .$colony) %>% lapply(check_values) %>%
-    unlist(use.names = FALSE)
+  add_HDI_mask <- function(colony_data) {
+    col <- colony_data$colony[1]
+    HDI_mask <- colony_data$x >= HDI_values[[col]][1] & colony_data$x <= HDI_values[[col]][2]
+    cbind(colony_data, data.frame(HDI_mask = HDI_mask))
+  }
 
-  set_colony_name <- . %>% as.data.frame(.) %>%
-    dplyr::rename_with(function(x) "slope", tidyselect::everything())  %>%
+  calculate_density <- function(colony_data){
+    dens <- density(colony_data$slope, n = 500)
+    data.frame(x = dens$x, y = dens$y, colony = colony_data$colony[1])
+  }
+
+  set_colony_name <- . %>% dplyr::rename_with(function(x) "slope", tidyselect::everything()) %>%
     dplyr::mutate(colony = map_chr(rownames(.), stri_extract, regex = "[0-9]{2}-[0-9]{2}"))
 
-  empirical_slopes <- empirical_slopes %>% unlist() %>% set_colony_name() %>%
-    dplyr::mutate(HDI_mask = add_hdi_mask(.), model = "empirical")
-  randomized_slopes <- randomized_slopes %>% unlist() %>% set_colony_name() %>%
-    dplyr::mutate(HDI_mask = add_hdi_mask(.),model = "null")
+  process_data <- . %>%
+    unlist() %>%
+    as.data.frame() %>%
+    set_colony_name() %>%
+    split(.$colony) %>%
+    walk(calculate_HDI) %>%
+    map(calculate_density) %>%
+    map(add_HDI_mask) %>%
+    do.call(rbind, .)
+
+  empirical_slopes <- empirical_slopes %>%
+    process_data() %>%
+    dplyr::mutate(model = "empirical")
+
+  randomized_slopes <- randomized_slopes %>%
+    process_data() %>%
+    dplyr::mutate(model = "null")
+
   plot_data <- rbind(empirical_slopes, randomized_slopes)
-  ggplot(plot_data, aes(x = slope)) +
-    geom_density(aes(lty = model)) +
-    facet_grid(.~colony)
+  ggplot(plot_data, aes(x = x, y =y )) +
+    geom_line(aes(lty = model)) +
+    geom_area(aes(x = x, y = y, group = model),
+              data = subset(plot_data, HDI_mask), alpha = 0.5) +
+    facet_grid(.~colony) +
+    labs(linetype = "Legend", x="Slope value",y="Count (density)")
 }

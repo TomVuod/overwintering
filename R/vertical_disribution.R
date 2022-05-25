c15e9622088eb84881f63ea2a58347f3f609f814
Tomasz Włodarczyk <uwb@Wlodarczyk.adm.uwb.edu.pl>
2022-05-25 17:43:16 +0200

    Regression of head width and maximal depth on the colony size

--- R/vertical_disribution.R
+++ R/vertical_disribution.R
@@ -1,7 +1,7 @@
 # assume uniform distribution of ants along the vertical axis
 expand_vert_distr <- function(rowline){
   if(any(is.na(rowline[,c("depth_lower_limit", "depth_upper_limit")]))) return(data.frame())
-  depths <- rowline$depth_upper_limit : (rowline$depth_lower_limit-1)
+  depths <- (rowline$depth_upper_limit+1): rowline$depth_lower_limit
   work_numb <- rowline$worker_number / (rowline$depth_lower_limit - rowline$depth_upper_limit)
   data.frame(depth = depths, worker_number = work_numb, colony = rowline$colony[1])
 }
@@ -22,3 +22,29 @@ aggr_vertical <- function(data = underground_distribution){
 }
 
 stratification_data <- aggr_vertical(underground_distribution)
+
+#' @importFrom purrr map_dfr keep
+# fit linear model of the relationship between log10 colony size and the maximal depth
+# reached by a colony
+# some little corrections have been made after publication so coefficients are now up to date
+maximal_depth_regr <- function(data = stratification_data, colony_metadata_ = colony_metadata){
+  max_depths <- stratification_data %>% split(.$colony) %>%
+    lapply(function(x) data.frame(colony = x$colony[1], depth = max(x$depth))) %>%
+    do.call(rbind,.)
+  col_sizes <- colony_metadata_ %>%
+    map_dfr(function(x) {data.frame(colony = x$colony_ID, size = log10(x$colony_size))})
+  model_data <- dplyr::inner_join(max_depths, col_sizes)
+  lm(depth~size, data = model_data)
+}
+
+# fit linear model of the relationship between log10 colony size and the maximal depth
+# mean head width of workers
+head_width_regr <- function(colony_metadata_ = colony_metadata){
+  plot_data <- colony_metadata_ %>%
+    map_dfr(function(x) data.frame(size = log10(x$colony_size),
+                                   mean_head_width = mean(x$head_width)/10^3,
+                                   colony = x$colony_ID))
+  # exclude the smallest colony (see publication)
+  plot_data <- dplyr::filter(plot_data, colony != "18-61")
+  lm(plot_data$mean_head_width ~ plot_data$size)
+}

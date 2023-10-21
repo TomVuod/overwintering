6c9ec92fc89eb7efd1cf99bcfdc216596696b72e
Tomek <tomwlo@gmail.com>
2023-10-21 21:09:32 +0200

    modification of datasets and adding missing queen depths

--- R/data.R
+++ R/data.R
@@ -10,7 +10,7 @@
 #'     }
 "vert_distribution"
 
-#' A data frame summarising ant removal experiment
+#' A data frame summarizing ant removal experiment across colonies
 #' @format Columns description:
 #'   \describe{
 #'     \item{N1}{number of workers from the upper part of the nest}
@@ -24,7 +24,7 @@
 #'     \item{removed total}{total number of removed workers}
 #'     \item{colony}{colony ID}
 #'   }
-"ant_removal"
+"colony_stats"
 
 #' Slopes obtained as a result of the permutation of the time series data
 "permutated_slopes"
@@ -34,26 +34,27 @@
 #' collected from the arena - they were active outside the nest.
 #' @format Columns description:
 #'  \describe{
-#'    \item{Date}{Collection date}
-#'    \item{Colony}{Colony ID}
+#'    \item{Date}{Collection date.}
+#'    \item{Colony}{Colony ID.}
 #'    \item{Unmarked}{Cumulative number of collected ants which have not
-#'    been marked}
-#'    \item{Red}{Cumulative number of the ants marked with red paint}
-#'    \item{Yellow}{Cumulative number of the ants marked with yellow paint}
-#'    \item{Dead_unm}{Cumulative number of the dead unmarked ants}
-#'    \item{Dead_red}{Cumulative number of the dead ants marked with red color}
+#'    been marked.}
+#'    \item{Red}{Cumulative number of the ants marked with red paint.}
+#'    \item{Yellow}{Cumulative number of the ants marked with yellow paint.}
+#'    \item{Dead_unm}{Cumulative number of the dead unmarked ants.}
+#'    \item{Dead_red}{Cumulative number of the dead ants marked with red color.}
 #'    \item{Dead_yellow}{Cumulative number of the dead ants marked with yellow
-#'     color}
+#'     color.}
 #'    \item{Removed_1}{Cumulative number of the removed marked ants from the upper
-#'    part of the nest}
+#'    part of the nest.}
 #'    \item{Removed_2}{Cumulative number of the removed marked ants from the lower
-#'    part of the nest}
+#'    part of the nest.}
 #'    \item{Upper_part_dead}{Cumulative number of the marked dead ants from the
-#'    upper part of the nest}
+#'    upper part of the nest.}
 #'    \item{Lower_part_dead}{Cumulative number of the marked dead ants from the
-#'    lower part of the nest}
+#'    lower part of the nest.}
 #'  }
-"experiment_course"
+"time_series_results"
+
 #' Colony metadata
 #'
 #' A list of additional data on the colonies
@@ -61,14 +62,14 @@
 #' @format Each list item contains the data on a sigle colony. This is a list made up
 #' of the following components:
 #' \describe{
-#'  \item{colony_ID}{The unique colony label}
-#'  \item{colony_size}{The number of workers in the colony}
+#'  \item{colony_ID}{The unique colony label.}
+#'  \item{colony_size}{The number of workers in the colony.}
 #'  \item{head_width}{A numeric vectors representing head widths of the sampled workers
-#'  in micrometers}
-#'  \item{measurement_source}{Indicates from which source ants devoted to be measured
-#'  were samples. 'Colony' means that ants were samples from directly from the living
-#'  population of the colony whereas 'corpses' means ants samples from dead individuals
-#'  collected after tranfering colonies to the laboratory}
+#'  in micrometers.}
+#'  \item{measurement_target}{Indicates the source from which measured ants were sampled.
+#'  'Colony' means that ants were sampled from directly from the living population of the
+#'  colony whereas 'corpses' means ants were sampled from dead individuals
+#'  collected after transfering colonies to the laboratory.}
 #'  \item{queens_depth}{A numeric vector indicating the depths at which queens have been found.
 #'  The vector length is equivalent to the number of queens.}
 #' }

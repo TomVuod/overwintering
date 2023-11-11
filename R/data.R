db1e10a1112a463144d4467f029ec75072b026aa
@tomwlo <tomwlo@gmail.com>
2023-11-11 18:34:43 +0100

    columns order changed

--- R/data.R
+++ R/data.R
@@ -1,18 +1,7 @@
-#' A list of data frames representing data on different colonies.
-#'
-#' @format Each data frame contains following columns:
-#'   \describe{
-#'     \item{colony}{colony ID}
-#'     \item{depth}{depth range, in cm}
-#'     \item{worker_number}{worker number within given depth range}
-#'     \item{cumulative_number}{sum of worker number from actual and
-#'       upper depth ranges}
-#'     }
-"vert_distribution"
-
 #' A data frame summarizing ant removal experiment across colonies
 #' @format Columns description:
 #'   \describe{
+#'     \item{colony}{colony ID}
 #'     \item{N1}{number of workers from the upper part of the nest}
 #'     \item{N2}{number of workers from the lower part of the nest}
 #'     \item{marked_1}{number of marked workers from the upper part of the nest}
@@ -22,7 +11,6 @@
 #'     \item{removed_2}{number of removed workers from the lower part of the nest
 #'     which have been marked}
 #'     \item{removed total}{total number of removed workers}
-#'     \item{colony}{colony ID}
 #'   }
 "colony_stats"
 
